0331970a8fb25dc093b35cdff772e3eaebc9027a
Panturas <rafigamer111@gmail.com>
2023-11-28 08:30:43 +0700

    penambahan alat

--- rkas/prosesalatpraktik.php
+++ rkas/prosesalatpraktik.php
@@ -1,5 +1,6 @@
 <?php
 include "koneksi.php";
+$sql = mysqli_query($conn, "SELECT * FROM tb_alat");
 if (isset($_POST['simpan'])) {
     $sumber_dana = $_POST['sumber_dana'];
     $tahun_ajuan = $_POST['tahun_ajuan'];
@@ -9,23 +10,23 @@ if (isset($_POST['simpan'])) {
     $harga = $_POST['harga'];
     $qty = $_POST['qty'];
     $kebutuhan = $_POST['kebutuhan'];
-}
-$query = "INSERT INTO tb_det_alat(sumber_dana, item, merk, spesifikasi, harga, qty, kebetuhan_untuk) VALUES ('$sumber_dana','$nama_item','$merk','$spesifikasi','$harga','$qty','$kebutuhan',)";
-$proses = mysql_query($conn, $query);
+    $proses =  mysqli_query($conn, "INSERT INTO tb_alat (sumber_dana, tahun_ajuan, item, merk, spesifikasi, harga, qty, kebutuhan_untuk) VALUES ('$sumber_dana','$tahun_ajuan','$nama_item','$merk','$spesifikasi', '$harga', '$qty', '$kebutuhan')");
+
 
 if ($proses) {
     echo "
     <script>
-    alert('Data Berhasil Ditambahkan');
-    window.location.href='prosesalatpraktik.php';
+        alert('Data Berhasil Masuk!');
+        window.location.href='alat_praktik.php';
     </script>
     ";
 } else {
     echo "
-    <script>
-    alert('Data Gagal Ditambahkan');
-    window.location.href='prosesalatpraktik.php';
-    </script>
-    ";
+<script>
+    alert('Data Gagal Masuk');  
+    window.location.href='alat_praktik.php';
+</script>
+";
+}
 }
 ?>
