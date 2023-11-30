0283709b180055ef907ba5b993802b87644be49d
Panturas <117643014+Panturas@users.noreply.github.com>
2023-11-30 21:28:13 +0700

    Update Logout aja

--- rkas/alat_bahan.php
+++ rkas/alat_bahan.php
@@ -398,7 +398,7 @@ $sql = mysqli_query($conn, "SELECT * FROM tb_bahan ORDER BY id_bahan DESC");
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="login.php">Logout</a>
                 </div>
             </div>
         </div>
