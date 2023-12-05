f1969f6ff2debd2f7810ab5fcb807ea57795e8e2
RdtGames12 <adt.ganteng1234@gmail.com>
2023-12-05 10:11:56 +0700

    filter alpha

--- rkas/lihatpengajuan.php
+++ rkas/lihatpengajuan.php
@@ -228,6 +228,10 @@ $sql1 = mysqli_query($conn, "SELECT * FROM tb_user WHERE id_user='$id'");
                         <div class="card shadow mb-4">
                         <div class="card-header py-3">
                             <h6 class="m-0 font-weight-bold text-primary">Daftar Ajuan</h6>
+                            <select name="ajuan">
+                                <option value="bahan">Ajuan Bahan</option>
+                                <option value="alat">Ajuan Alat</option>
+                            </select>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
