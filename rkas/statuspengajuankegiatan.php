18aefc521aad1ed5a6e80dd456a0f3b06a7dd7ac
RdtGames12 <adt.ganteng1234@gmail.com>
2024-02-13 07:29:04 +0700

    update

--- rkas/statuspengajuankegiatan.php
+++ rkas/statuspengajuankegiatan.php
@@ -99,7 +99,7 @@ $sql1 = mysqli_query($conn, "SELECT * FROM tb_user WHERE id_user='$id'");
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Jenis Ajuan:</h6>
                         <a class="collapse-item" href="statuspengajuankegiatan.php?id=<?= $id ?>">Kegiatan</a>
-                        <a class="collapse-item" href="#">Sarana</a>
+                        <a class="collapse-item" href="statuspengajuansarana.php?id=<?= $id ?>">Sarana</a>
                     </div>
                 </div>
             </li>
