c8f2fb9eba11297e119a654cff972e894e8d4394
RdtGames12 <adt.ganteng1234@gmail.com>
2024-02-13 13:55:43 +0700

    validator sarana

--- rkas/validatorkegiatan.php
+++ rkas/validatorkegiatan.php
@@ -181,7 +181,7 @@ if ($id == 702205615) {
                         </div>
                         <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Daftar Ajuan</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Daftar Ajuan Kegiatan</h6>
                         </div>
                         <div class="card-body">
                         <div class="table-responsive">
