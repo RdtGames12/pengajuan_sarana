40e874642ac3f5e12a48e47bda56ea7fd4efeccc
Panturas <rafigamer111@gmail.com>
2024-05-21 08:12:24 +0700

    ROR

--- rkas/loginproses.php
+++ rkas/loginproses.php
@@ -6,68 +6,59 @@ if (isset($_POST['simpan'])) {
     $user = $_POST['user'];
     $pass = $_POST['password'];
     
-    $stmt = $conn->prepare("SELECT * FROM tb_user WHERE nama_user = ?");
-    $stmt->bind_param("s", $user);
+    $stmt = $conn->prepare("SELECT * FROM tb_user WHERE nama_user='$user' AND password='$pass'");
     $stmt->execute();
     $result = $stmt->get_result();
     
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         
-        if (password_verify($pass, $row['password'])) {
-            $id = $row['id_bagian'];
-            $_SESSION['id'] = $row['id_user'];
-            
-            switch ($id) {
-                case 1:
-                    echo "
-                    <script>
-                        alert('Login Berhasil');
-                        window.location.href='homejurusan.php';
-                    </script>";
-                    break;
-                case 2:
-                    echo "
-                    <script>
-                        alert('Login Berhasil');
-                        window.location.href='homewakepsek.php?id={$row['id_user']}';
-                    </script>";
-                    break;
-                case 3:
-                    echo "
-                    <script>
-                        alert('Login Berhasil');
-                        window.location.href='hometu.php?id={$row['id_user']}';
-                    </script>";
-                    break;
-                case 10:
-                    echo "
-                    <script>
-                        alert('Login Berhasil');
-                        window.location.href='admin.php?id={$row['id_user']}';
-                    </script>";
-                    break;
-                case 11:
-                    echo "
-                    <script>
-                        alert('Login Berhasil');
-                        window.location.href='homevalidator.php?id={$row['id_user']}';
-                    </script>";
-                    break;
-                default:
-                    echo "
-                    <script>
-                        alert('Invalid user role');
-                        window.location.href='index.php';
-                    </script>";
-                    break;
-            }
-        } else {
-            echo "
-            <script>
-                alert('Username atau Password Tidak Ditemukan');
-                window.location.href='index.php';
-            </script>";
+        $id = $row['id_bagian'];
+        $_SESSION['id'] = $row['id_user'];
+        
+        switch ($id) {
+            case 1:
+                echo "
+                <script>
+                    alert('Login Berhasil');
+                    window.location.href='homejurusan.php?id={$row['id_user']}';
+                </script>";
+                break;
+            case 2:
+                echo "
+                <script>
+                    alert('Login Berhasil');
+                    window.location.href='homewakepsek.php?id={$row['id_user']}';
+                </script>";
+                break;
+            case 3:
+                echo "
+                <script>
+                    alert('Login Berhasil');
+                    window.location.href='hometu.php?id={$row['id_user']}';
+                </script>";
+                break;
+            case 10:
+                echo "
+                <script>
+                    alert('Login Berhasil');
+                    window.location.href='admin.php?id={$row['id_user']}';
+                </script>";
+                break;
+            case 11:
+                echo "
+                <script>
+                    alert('Login Berhasil');
+                    window.location.href='homevalidator.php?id={$row['id_user']}';
+                </script>";
+                break;
+            default:
+                echo "
+                <script>
+                    alert('Invalid user role');
+                    window.location.href='index.php';
+                </script>";
+                break;
         }
     } else {
         echo "
